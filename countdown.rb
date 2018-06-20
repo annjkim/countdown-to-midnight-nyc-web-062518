3773ae5586f4b2984c7d4b4de9757a9b7d6d26ac
Ann Kim <annkim822@gmail.com>
2018-06-20 04:16:32 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,9 @@
 #write your code here
 
 def countdown(integer)
-  number = 0
-  while number < 10
+  while integer > 10
     puts "#{number} SECOND(S)!"
-    number -= 1
+    integer -= 1
   end
   return "HAPPY NEW YEAR!"
 end
